42e4a0977b7ee46fb54a9b49bbca9dba8dbc9440
Panhaka <limpanhaka@gmail.com>
2025-01-11 10:41:56 +0700

    Create Chaingselectors.js

---
+++ Chaingselectors.js
@@ -0,0 +1,9 @@
+<script>
+  $(document).ready(function () {
+    // Chain methods for concise and efficient code
+        $('#main)            // Select the #main element
+          .find('p') // Find all <p> elements inside #main
+          .addClass('active') //Add 'active' class to all those <p> elements
+          .fadeIN(); //Fade them in
+  });
+</script>
