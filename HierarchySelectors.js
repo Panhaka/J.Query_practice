e382cb5b9bd5965310e928981c5e9ac4ceaef3a0
Panhaka <limpanhaka@gmail.com>
2025-01-11 10:23:31 +0700

    Create HierarchySelectors.js

---
+++ HierarchySelectors.js
@@ -0,0 +1,7 @@
+<script> 
+  // Select elements based on their relationships in the DOM.
+  $('div p');   // Select all <p> inside <div>
+$('ul > li');    //Select all direct child <li> of <ul>
+$('div + p');    //Select the <p> immediately after a <div>
+
+</script>
